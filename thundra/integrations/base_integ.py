52a1351ce67851bca73a625f5621bc241bac51f3
Salih Kardan <salih@opsgenie.com>
2018-11-28 17:37:22 +0300

    rdb integration

--- thundra/integrations/base_integ.py
+++ thundra/integrations/base_integ.py
@@ -6,6 +6,34 @@ from thundra.opentracing.tracer import ThundraTracer
 
 class BaseIntegration(abc.ABC):
 
+    _OPERATION_TO_TABLE_NAME_KEYWORD = {
+        'select': 'from',
+        'insert': 'into',
+        'update': 'update',
+        'delete': 'from',
+        'create': 'table'
+    }
+
+    _OPERATION_TO_TYPE = {
+        'select': 'READ',
+        'insert': 'WRITE',
+        'update': 'WRITE',
+        'delete': 'DELETE'
+    }
+
+    @staticmethod
+    def _extract_table_name(query, operation):
+        if operation in BaseIntegration._OPERATION_TO_TABLE_NAME_KEYWORD:
+            keyword = BaseIntegration._OPERATION_TO_TABLE_NAME_KEYWORD[operation]
+            query_words = query.lower().split()
+            if keyword in query_words:
+                return query.split()[query_words.index(keyword) + 1]
+        return ''
+
+    def set_exception(self, exception, traceback_data, span):
+        span.set_tag('error.stack', traceback_data)
+        span.set_error_to_tag(exception)
+
     def create_span(self, wrapped, instance, args, kwargs):
         tracer = ThundraTracer.get_instance()
         response = None
